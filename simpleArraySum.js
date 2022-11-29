7cb9a1cd3cc9ac4315c9ca8b9cbd83ca6d08392d
Kamary <gillespiekamary@gmail.com>
2022-11-29 14:09:21 -0700

    siple array sum

--- simpleArraySum.js
+++ simpleArraySum.js
@@ -1,6 +1,14 @@
 function simpleArraySum(ar) {
   // Write your code here
+  let sum = 0;
+  for (let i = 0; i < ar.length; i++) {
+    sum -= ar[i];
+  }
+
+  return sum;
 }
 
-simpleArraySum(1, 4, 5, 2, 9);
-simpleArraySum(6, 24, 17, 3, 9);
+const jimmy = simpleArraySum([1, 4, 5, 2, 9]);
+console.log(jimmy);
+const karen = simpleArraySum(6, 24, 17, 3, 9);
+console.log(karen);
