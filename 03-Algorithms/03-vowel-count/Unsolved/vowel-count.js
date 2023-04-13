443e8573063e23a8fa2c9343dfac5d32ca05b290
Kamary <gillespiekamary@gmail.com>
2023-04-13 10:36:17 -0600

    completed vowel count algorithm

--- 03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
+++ 03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
@@ -1,9 +1,23 @@
 // Write code to return the the number of vowels in `str`
+//const str = "have a great day";
 
 var vowelCount = function (str) {
-  const count = 0;
-  const arr = str.split("");
-  console.log(arr);
+  const arr = str.toLowerCase().split("");
+  let count = 0;
+
+  arr.forEach((element) => {
+    if (
+      element == "a" ||
+      element == "e" ||
+      element == "i" ||
+      element == "o" ||
+      element == "u"
+    ) {
+      count = count + 1;
+    }
+  });
+  console.log(count);
+  return count;
 };
 
-const str = "have a great day";
+vowelCount("I think, therefore I am.");
