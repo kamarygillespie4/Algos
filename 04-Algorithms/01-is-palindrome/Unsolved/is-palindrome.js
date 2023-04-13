6e875e7d2ec81c52b601badd3c77a58ce8f9cfa3
Kamary <gillespiekamary@gmail.com>
2023-04-13 11:19:03 -0600

    completed is palindrome algo

--- 04-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
+++ 04-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,13 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function (str) {
+  const rev = str.split("").reverse().join("");
+  console.log(rev);
+
+  if (str !== rev) {
+    console.log("false");
+    return false;
+  } else {
+    return true;
+  }
+};
