3a13265748f3255d937541a8065948b15ed1f6fd
Kamary <gillespiekamary@gmail.com>
2023-04-17 10:06:23 -0600

    factorial algorithm

--- 04-Algorithms/02-factorial/Unsolved/factorial.js
+++ 04-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,9 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function (num) {
+  let sum = 1;
+  for (var i = 1; i <= num; i++) {
+    sum *= i;
+  }
+  return sum;
+};
